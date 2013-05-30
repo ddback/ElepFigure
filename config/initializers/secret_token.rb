f93232c1871c94a9e569cd6eb7da1bc4e807071f
huwence <huwence@gmail.com>
2013-05-30 18:26:19 +0800

    rails new ElepFigure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElepFigure::Application.config.secret_token = 'c3c364973914dc760b988d0081025420ca7e735a802d4c7b942a13f1ba96e4f759035523d395b7a5b9d3fb8971498529774cfa28ac9724ad8484745432ec4747'
